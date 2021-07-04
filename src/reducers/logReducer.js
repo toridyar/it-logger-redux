c60410c5b97d4f6736c8a172a3724d8c65970312
toridyar <courtney.l.ledford@gmail.com>
2021-07-04 13:10:07 -0400

    add redux

---
+++ src/reducers/logReducer.js
@@ -0,0 +1,33 @@
+import { GET_LOGS, SET_LOADING, LOGS_ERROR } from '../actions/types'
+
+const initialState = {
+    logs: null,
+    current: null,
+    loading: false,
+    error: null
+}
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (state = initialState, action) => {
+    switch (action.type) {
+        case SET_LOADING:
+            return {
+                ...state,
+                loading: true
+            };
+        case LOGS_ERROR:
+            console.error(action.payload)
+            return {
+                ...state,
+                error: action.payload
+            };
+        case GET_LOGS:
+            return {
+                ...state,
+                logs: action.payload,
+                loading: false
+            }
+        default:
+            return state;
+    }
+}
