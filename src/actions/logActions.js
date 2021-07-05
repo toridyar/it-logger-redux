817dbfe967228a6fa307d713c957282ab1407197
toridyar <courtney.l.ledford@gmail.com>
2021-07-05 12:14:32 -0400

    moved more of logs into redux

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,5 +1,6 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR } from './types'
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG } from './types'
 
+// Get logs from server
 export const getLogs = () => {
     return async (dispatch) => {
         try {
@@ -18,11 +19,63 @@ export const getLogs = () => {
                 payload: error.response.data
             })
         }
-
-
-
     }
 }
+
+// Add new log
+export const addLog = (log) => {
+    return async (dispatch) => {
+        try {
+            setLoading();
+
+            const res = await fetch('/logs', {
+                method: 'POST',
+                body: JSON.stringify(log),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            });
+            const data = await res.json();
+
+            dispatch({
+                type: ADD_LOG,
+                payload: data
+            })
+        } catch (error) {
+            dispatch({
+                type: LOGS_ERROR,
+                payload: error.response.data
+            })
+        }
+    }
+}
+
+// Delete log from server
+export const deleteLog = (id) => {
+    console.log(id)
+    return async (dispatch) => {
+
+        try {
+            setLoading();
+
+            const res = await fetch(`/logs/${id}`, {
+                method: 'DELETE',
+            });
+
+            const data = await res.json();
+
+            dispatch({
+                type: DELETE_LOG,
+                payload: id
+            });
+        } catch (err) {
+            dispatch({
+                type: LOGS_ERROR,
+                payload: err.response.statusText
+            });
+        }
+    }
+};
 
 //Set loading to true
 export const setLoading = () => {
